e0341a12c127463e33c37de0921767813fba617a
Faisal-bot886 <indrasukmana764@gmail.com>
2025-05-09 13:23:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const menu = require("./lib/menu.js")
 //Setting
 
 const apivhtear = 'Apikey vhtear';
-const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const apibarbar = 'Apikey kontol';
+const BotName = 'FAIS BOT'; 
+const instagram = 'kosong'; 
 const aktif = 'Kapan bot aktif';
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+const vcard = 'BEGIN:VCARD\n' // 
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // Faisal
+            + 'ORG:VVIPINDO;\n' // Fais bot
+            + 'TEL;type=CELL;type=VOICE;waid=6282311994702:+62 831-3790-8353\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
